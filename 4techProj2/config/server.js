53d679ae850eebaae06f80c1306ca92100b22595
Gabriel Fronio <gabrielfronio@gmail.com>
2018-07-19 16:47:49 -0300

    pequena mudanca

--- 4techProj2/config/server.js
+++ 4techProj2/config/server.js
@@ -3,7 +3,6 @@
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
-const path = require('path')
 const consign = require('consign')
 
 server.use('/vjobs', express.static(__dirname + '/app/static'))
