05d52d881bd53303815f08b3ae48d6c3f1997f52
Gabriel Fronio <gabrielfronio@gmail.com>
2018-07-20 12:51:10 -0300

    arrumado

--- 4techProj2/config/server.js
+++ 4techProj2/config/server.js
@@ -5,7 +5,7 @@ const server = express()
 const bodyParser = require('body-parser')
 const consign = require('consign')
 
-server.use('/vjobs', express.static(__dirname + '/app/static'))
+server.use('/vjobs', express.static(__dirname + '/../app/static'))
 server.use(bodyParser.urlencoded({extended: false}))
 server.use(bodyParser.json())
 consign().include('./app/routes').into(server)
