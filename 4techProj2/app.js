184bf70deef417f28af8907dcb489627735a5cc4
Gabriel Fronio <gabrielfronio@gmail.com>
2018-07-28 09:30:55 -0300

    Mudanças para o heroku

--- 4techProj2/app.js
+++ 4techProj2/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const server = require('./config/server')
-const port = 3001
+const port = process.env.PORT || 3001
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
